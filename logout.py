a383a7db02557dd28aea7c4eb8ef60ba8e25c31e
shreetiranjit <ranjit.shreeti1@gmail.com>
2021-09-03 09:28:57 +0545

    Log out

--- logout.py
+++ logout.py
@@ -9,6 +9,7 @@ def logout():
     response = messagebox.askyesno("Logout Application","Are you sure want to close this application")
     if response > 0:
         root.destroy()
+
         return
 mybutton = Button(text="Logout", height="3", width="30", font=("CALIBRI", 12, "bold"), fg="BLACK", bg="#BC85A3",  command=logout).pack()
 root.mainloop()
