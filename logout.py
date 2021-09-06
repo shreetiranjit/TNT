6fc0ac6b96663f88f403117b97a0fac051dfd746
shreetiranjit <ranjit.shreeti1@gmail.com>
2021-09-06 20:36:36 +0545

    Log out

--- logout.py
+++ logout.py
@@ -10,6 +10,5 @@ def logout():
     if response > 0:
         root.destroy()
 
-        return
 mybutton = Button(text="Logout", height="3", width="30", font=("CALIBRI", 12, "bold"), fg="BLACK", bg="#BC85A3",  command=logout).pack()
 root.mainloop()
